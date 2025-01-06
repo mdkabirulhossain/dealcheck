30658b901e74facb846c7156642f2b56b3bc2fcb
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-01-06 11:21:52 +0600

    Write Basic Information about Redux in Notes.js File

---
+++ src/Notes.js
@@ -0,0 +1,13 @@
+// What is Redux? 
+// = Redux is a flexible State Container for JavaScript Apps that manages our application state separately 
+
+// Redux user kunu command taht means click it's called Action
+// After action all the logic write in reducer.
+
+//Reducer Structure
+// reducer(state, action){
+//    return newState;
+// }
+
+//We basically use Redux to easily maintain state;
+
